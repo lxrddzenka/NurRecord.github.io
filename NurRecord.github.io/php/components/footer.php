a92c45e0e54550037de13e5ff2749fdd06e42780
lxrddzenka <114692081+lxrddzenka@users.noreply.github.com>
2022-10-07 14:07:25 +0300

    Add files via upload

---
+++ NurRecord.github.io/php/components/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer" id="footer">
+              <div class="wrapper">
+                <div class="footer-wrap">
+                  <div>
+                    <ul class="footer-info">
+                      <li class="footer-info_item" style="color: white">Стрюк Евгений Михайлович</li>
+                      <li class="footer-info_item" style="color: white">jenya.stryuk@yandex.ru</li>
+                      <li class="footer-info_item" style="color: white">+7 (916)-368-78-48</li>
+                      <li class="footer-info_item" style="color: white"><?php echo 'Сформировано ', $today, " в ", $time?></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </footer>
+        </body>
+</html>
